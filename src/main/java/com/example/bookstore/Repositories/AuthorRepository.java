d9ba6e560314b606d5129afc7efbb39befb2a42b
Niringiye Allan Smith <trizey14@gmail.com>
2024-04-18 17:45:36 +0300

    🏁 Changes to the Repositories 🏁

--- src/main/java/com/example/bookstore/Repositories/AuthorRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.bookstore.Repositories;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.bookstore.Models.Author;
-
-public interface AuthorRepository extends JpaRepository<Author,Long>{
-    
-}
--- src/main/java/com/example/bookstore/Repositories/DateAuditRepository.java
+++
@@ -1,5 +0,0 @@
-package com.example.bookstore.Repositories;
-
-public class DateAuditRepository {
-    
-}
